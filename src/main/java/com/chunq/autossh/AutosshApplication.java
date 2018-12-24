d919fbd2ac9a50776b2d047f9dc436b76da417ef
NewHms <37338418+NewHms@users.noreply.github.com>
2018-12-24 17:48:00 +0800

    框架1.0

---
+++ src/main/java/com/chunq/autossh/AutosshApplication.java
@@ -0,0 +1,14 @@
+package com.chunq.autossh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutosshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutosshApplication.class, args);
+    }
+
+}
+
